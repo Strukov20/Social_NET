d81d6a053dad8ee22b420085a4507f254c87a6f0
Bohdan Strukov <bodya19981997@gmail.com>
2020-12-05 13:30:39 +0200

    Updates

---
+++ precache-manifest.7945263daef13c097e40f5b177c50567.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a62e3d4aa5ebe5cf5ca2d5a9d3450f1",
+    "url": "/Social_NET/index.html"
+  },
+  {
+    "revision": "7f8649945d8cdb951c4e",
+    "url": "/Social_NET/static/css/3.24a4b70e.chunk.css"
+  },
+  {
+    "revision": "799a135e949a871e45f6",
+    "url": "/Social_NET/static/css/4.5c65787b.chunk.css"
+  },
+  {
+    "revision": "1d266f5e47a30d2b2de4",
+    "url": "/Social_NET/static/css/main.4ec980e8.chunk.css"
+  },
+  {
+    "revision": "6b290164fae1b05e2778",
+    "url": "/Social_NET/static/js/2.a1321644.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Social_NET/static/js/2.a1321644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f8649945d8cdb951c4e",
+    "url": "/Social_NET/static/js/3.ef1abc5e.chunk.js"
+  },
+  {
+    "revision": "799a135e949a871e45f6",
+    "url": "/Social_NET/static/js/4.9999f18e.chunk.js"
+  },
+  {
+    "revision": "1d266f5e47a30d2b2de4",
+    "url": "/Social_NET/static/js/main.eaa11d9c.chunk.js"
+  },
+  {
+    "revision": "d1fb38d8e5a01dd60b70",
+    "url": "/Social_NET/static/js/runtime-main.3104f035.js"
+  },
+  {
+    "revision": "cd9f6d3ad366512f4ee6169368f7efee",
+    "url": "/Social_NET/static/media/auf_mp3.cd9f6d3a.mp3"
+  },
+  {
+    "revision": "80d6e3b97606e418e71ddc9b32431ac1",
+    "url": "/Social_NET/static/media/avatar.80d6e3b9.svg"
+  },
+  {
+    "revision": "f44b5b1890cd15ce9970f49e1b106ec7",
+    "url": "/Social_NET/static/media/idris_amp_leos_-_vidu_ne_podam.f44b5b18.mp3"
+  },
+  {
+    "revision": "39844780155658ff47cc9373e44f9b1c",
+    "url": "/Social_NET/static/media/islam_itljashev_-_ona_ljubila_rozi.39844780.mp3"
+  },
+  {
+    "revision": "6a0b8f448e8bff6515b8e0e5a11daccc",
+    "url": "/Social_NET/static/media/login.6a0b8f44.svg"
+  },
+  {
+    "revision": "2341e14bfcc8e06fddb2d0051a153f17",
+    "url": "/Social_NET/static/media/logout.2341e14b.svg"
+  },
+  {
+    "revision": "508321d79e6ab81760621a28169e994f",
+    "url": "/Social_NET/static/media/preloader.508321d7.svg"
+  },
+  {
+    "revision": "77d9312a554f7e61aa44ff063aeaf633",
+    "url": "/Social_NET/static/media/user_img.77d9312a.svg"
+  }
+]);
