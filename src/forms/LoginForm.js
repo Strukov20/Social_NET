6a74a4ca4602063718d0567f80c520c8844012bc
Bohdan Strukov <bodya19981997@gmail.com>
2020-12-08 19:38:05 +0200

    add captcha

--- src/forms/LoginForm.js
+++ src/forms/LoginForm.js
@@ -4,12 +4,14 @@ import {InputArea} from "../components/common/FormsControls/LoginFormControls";
 import {required} from "../utils/validators/validators";
 import {CreateField} from "../components/common/FormsControls/CreateField";
 
-const LoginForm = ({handleSubmit, error}) => {
+const LoginForm = ({handleSubmit, error, captchaUrl}) => {
     return (
         <form onSubmit={handleSubmit}>
             {CreateField("Email", 'email', [required], InputArea, {type: 'email'})}
             {CreateField("Password", 'password', [required], InputArea, {type: 'password'})}
             {CreateField('checkbox', 'rememberMe', null, 'input', {type: 'checkbox'}, 'rememberMe',  "login_form_checkbox")}
+            {captchaUrl && <img src={captchaUrl}/>}
+            {captchaUrl && CreateField("Captcha", 'captcha', null, InputArea, {}) }
             { error &&
                 <div className="login_form_error">
                     {error}
